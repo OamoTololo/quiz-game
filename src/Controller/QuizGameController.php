d6fd018818e84779e21d199512392a934c41f883
Oamogetswe <oamotebogomgidi@gmail.com>
2026-02-20 23:20:49 +0200

    Created three functions with routes for each page of the quiz game.

--- src/Controller/QuizGameController.php
+++ src/Controller/QuizGameController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class QuizGameController extends AbstractController
 {
-    #[Route('/', name: 'app_quiz_game')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
     {
-        return $this->render('quiz_game/index.html.twig', [
-            'controller_name' => 'QuizGameController',
-        ]);
+        return $this->render('quiz_game/home.html.twig');
     }
+
+//    #[Route("/questions", name: 'app_questions')]
+//    public function questions(): Response
+//    {
+//        return $this->render('quiz_game/questions.html.twig');
+//    }
+//
+//    #[Route("/results", name: 'app_results')]
+//    public function results(): Response
+//    {
+//        return $this->render('quiz_game/results.html.twig');
+//    }
 }
